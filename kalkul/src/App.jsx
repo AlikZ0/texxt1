7190f8d7fda1d2386a114048f515bfd08023b43a
AlikZ0 <zalinov96@gmail.com>
2022-06-16 21:25:20 +0400

    tes5

---
+++ kalkul/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import InputG from './inputG';
+
+function App() {
+  return (
+   <div className='G-center'>
+        <InputG />
+   </div>
+  );
+}
+
+export default App;
---
+++ kalkul/src/inputG/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./slyle.css"
+
+class InputG extends React.Component {
+
+    state = {
+        number: 0,
+        numberValue: 0
+    }
+    increment = () => {
+        if (this.state.numberValue) {
+            this.setState({ number: this.state.number + +this.state.numberValue })
+        } else {
+            this.setState({ number: ++this.state.number })
+        }
+    }
+    decrement = () => {
+        if (this.state.numberValue) {
+            this.setState({ number: this.state.number - +this.state.numberValue })
+        } else {
+            this.setState({ number: --this.state.number })
+        }
+    }
+    foo = (e) => {
+        this.setState({ numberValue: e.target.value })
+
+    }
+    render() {
+        return <div>
+            <input type="namber" value={this.state.numberValue} onChange={this.foo} />
+            <span className="G-span" onClick={this.decrement}>-</span>
+            <span className="G-span">{this.state.number}</span>
+            <span className="G-span" onClick={this.increment}>+</span>
+        </div>
+    }
+}
+
+
+
+
+export default InputG 
